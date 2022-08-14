213536442f8dae505a7d0d2f66182e34c7b22df2
chris-hellon <38248279+chris-hellon@users.noreply.github.com>
2022-08-14 13:07:58 +0700

    Delete PaperStreetSoap/Models directory

--- PaperStreetSoap/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaperStreetSoap.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
